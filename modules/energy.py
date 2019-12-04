c41c37369562b81d8a9c8f9d118b2dd596e8e240
Nikita <koritcky@gmail.com>
2019-12-04 21:21:13 +0300

    completed functions

--- modules/energy.py
+++ modules/energy.py
@@ -14,6 +14,7 @@ def Heff (theta, h):
 
 
 def dHeff(theta, h):
+    """derivative of Heff"""
     return (l * Md * np.sin(theta))/(Heff(theta, h))
 
 
@@ -21,13 +22,23 @@ def cos_theta_f(theta, h):
     return (h - l * Md * np.cos(theta))/Heff(theta, h)
 
 
+def d_cos_theta_f(theta, h):
+    """derivative of cos_theta_f"""
+    return (l * Md * np.sin(theta))/Heff(theta, h) - \
+           (h - l * Md * np.cos(theta)) * dHeff(theta, h)/(Heff(theta, h) ** 2)
+
+
 def energy(theta, *args):
     x, h = args
     return (-x * Mf * Heff(theta, h) -
-           Md * h * (1-x) * np.cos(theta) -
-           Kd * (1-x) * np.cos(theta) ** 2 -
+           Md * h * (1 - x) * np.cos(theta) -
+           Kd * (1 - x) * np.cos(theta) ** 2 -
            Kf * x * cos_theta_f(theta, h) ** 2) * 10 ** 14
 
 
 def denergy(theta, *args):
     x, h = args
+    return (-x * Mf * dHeff(theta, h) +
+            Md * h * (1 - x) * np.sin(theta) +
+            Kd * (1 - x) * np.sin(2 * theta) -
+            Kf * x * 2 * cos_theta_f(theta, h) * d_cos_theta_f(theta, h)) * 10 ** 14
